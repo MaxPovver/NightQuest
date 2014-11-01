51b5851dfc755b771df8090556e501a739529627
MaxPovver <chistov_maxim@mail.ru>
2014-11-01 11:30:01 +0300

    Разделил все действия на отдельные экраны

---
+++ NightQuest/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  NightQuest
+//
+//  Created by Admin on 01.11.14.
+//  Copyright (c) 2014 NightQuest. All rights reserved.
+//
+
+import UIKit
+
+
+class MainViewController: UIViewController {//контроллер главного окна проги
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
